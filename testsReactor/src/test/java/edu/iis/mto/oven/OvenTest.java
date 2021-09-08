36262136d3fe8d574ce619dbb598d07de2b463bf
MateuszRoslak <222554@edu.p.lodz.pl>
2021-09-08 17:24:25 +0200

    Test variables initialization, first test implemented

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -4,12 +4,49 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
+
+    @Mock
+    HeatingModule heatingModule;
+
+    @Mock
+    Fan fan;
+
+    Oven oven;
+
+    BakingProgram bakingProgram;
+
+    List<ProgramStage> programStages = List.of(
+            ProgramStage.builder()
+                    .withStageTime(180)
+                    .withHeat(HeatType.THERMO_CIRCULATION)
+                    .withTargetTemp(220)
+                    .build(),
+            ProgramStage.builder()
+                    .withStageTime(30)
+                    .withHeat(HeatType.GRILL)
+                    .withTargetTemp(220)
+                    .build()
+    );
+
+    @BeforeEach
+    void setUp() {
+        oven = new Oven(heatingModule, fan);
+        bakingProgram = BakingProgram.builder()
+                .withInitialTemp(100)
+                .withStages(programStages)
+                .build();
+    }
 
     @Test
     void itCompiles() {
@@ -18,7 +55,7 @@ class OvenTest {
 
     @Test
     void shouldCreateOvenObjectWithoutThrowingException() {
-        fail("unimplemented");
+        Assertions.assertNotNull(oven);
     }
 
     @Test
