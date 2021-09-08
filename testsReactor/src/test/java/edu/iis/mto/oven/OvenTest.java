0cabc0d0282243364d82a7499ea3e244a02ce4b3
MateuszRoslak <222554@edu.p.lodz.pl>
2021-09-08 17:15:53 +0200

    Test cases designed

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -2,6 +2,7 @@ package edu.iis.mto.oven;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,4 +16,33 @@ class OvenTest {
         assertThat(true, equalTo(true));
     }
 
+    @Test
+    void shouldCreateOvenObjectWithoutThrowingException() {
+        fail("unimplemented");
+    }
+
+    @Test
+    void shouldThrowHeatingExceptionWhenProblemWithThermalCircuitOccurred() throws HeatingException {
+        fail("unimplemented");
+    }
+
+    @Test
+    void shouldNotInvokeOvenFanWhenBakingWithThermalCircuitOff() {
+        fail("unimplemented");
+    }
+
+    @Test
+    void shouldStartBakingWithCorrectTemperature() {
+        fail("unimplemented");
+    }
+
+    @Test
+    void shouldInvokeHeatMethodWhenBakingWithHeater() {
+        fail("unimplemented");
+    }
+
+    @Test
+    void shouldInvokeGrillMethodWhenBakingWithGrill() {
+        fail("unimplemented");
+    }
 }
